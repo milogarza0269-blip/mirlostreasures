3b5c9520c691d5d2749420c4b93b8f0956d1e59a
milogarza0269-blip <milogarza0269@gmail.com>
2025-11-29 14:42:22 -0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id: "michael-kors-colorblock-pebbled-leather-satchel",
+        title: "👜 MICHAEL KORS Colorblock Pebbled Leather Satchel",
+        price: 150,
+        category: "purses",
+        description: "Sophisticated and structured, this MICHAEL KORS satchel features a timeless colorblock design in black and cream.",
+        condition: "new",
+        image: "images/mkpanda-1.jpg",
+        images: [
+            "images/mkpanda-1.jpg",
+            "images/mkpanda-2.jpg",
+            "images/mkpanda-3.jpg",
+            "images/mkpanda-4.jpg"
+        ],
+        featured: true,
+        inventory: 1
+    }
+];
